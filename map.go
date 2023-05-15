cb66e5833abb59914320fd01062fdb173a33cda1
Акматалиев Бекмамат Маматалиевич <b.akmataliev@tinkoff.ru>
2023-05-15 13:00:34 +0300

    map with pointer

--- map.go
+++ map.go
@@ -3,8 +3,18 @@ package slice
 func Map[A, B any](arr []A, lambda func(A) B) []B {
 	refArr := make([]B, len(arr))
 
-	for i, t := range arr {
-		refArr[i] = lambda(t)
+	for i := range arr {
+		refArr[i] = lambda(arr[i])
+	}
+
+	return refArr
+}
+
+func MapPtr[A, B any](arr []A, lambda func(*A) B) []B {
+	refArr := make([]B, len(arr))
+
+	for i := range arr {
+		refArr[i] = lambda(&arr[i])
 	}
 
 	return refArr
