d6fe6e8f455c1a57bbb87cf34cada3360a63cf9c
Bek <b.akmataliev@tinkoff.ru>
2023-08-14 14:59:03 +0300

    ReducePtr

--- reduce.go
+++ reduce.go
@@ -7,3 +7,11 @@ func Reduce[A, B any](acc A, arr []B, lambda func(a A, b B) A) A {
 
 	return acc
 }
+
+func ReducePtr[A, B any](acc A, arr []B, lambda func(a A, b *B) A) A {
+	for i := range arr {
+		acc = lambda(acc, &arr[i])
+	}
+
+	return acc
+}
